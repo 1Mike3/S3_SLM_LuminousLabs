948418e4c6845a324b68f2ddea44e66f2c89cd33
srebl <sebastian_rath23@protonmail.com>
2024-01-06 22:07:41 +0100

    undo deletions

---
+++ src/main/java/game/Board.java
@@ -0,0 +1,8 @@
+package game;
+
+interface Board {
+    boolean addMove(Player player, int x, int y);
+    char[][] getBoardValues();
+    BoardState getBoardState();
+    PlayerState getPlayerState(Player player);
+}
---
+++ src/main/java/game/HumanPlayer.java
@@ -0,0 +1,24 @@
+package game;
+
+import common.Result;
+
+class HumanPlayer implements Player {
+    HumanPlayer(GameIO io, String name, char mark){
+
+    }
+
+    @Override
+    public char getMark() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Result<Boolean, String> makeMove(Board board) {
+        return null;
+    }
+}
