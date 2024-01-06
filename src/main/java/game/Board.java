1b38da8db50c07d8210514a1850034f5c5c8d9b6
RefRexi <119524317+RefRexi@users.noreply.github.com>
2024-01-06 20:43:16 +0100

    Delete src/main/java/game/Board.java

--- src/main/java/game/Board.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-interface Board {
-    boolean addMove(Player player, int x, int y);
-    char[][] getBoardValues();
-    BoardState getBoardState();
-    PlayerState getPlayerState(Player player);
-    public void printBoard();
-}
