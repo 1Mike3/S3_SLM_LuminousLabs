1897442b787d425df2d605f281824906d8c8d8ae
Emad <ic22b050@technikum-wien.at>
2024-01-06 15:37:33 +0100

    Revised different implementation errors in Board_3x3 Redid Board_3x3Test.java Changed HumanPlayer to return false instead of throwing an error

--- src/main/java/game/Board_3x3.java
+++ src/main/java/game/Board_3x3.java
@@ -1,51 +1,31 @@
-
 package game;
 
-    class Board_3x3 implements Board {
-        private char[][] board;
-        private BoardState boardState;
-        private PlayerState playerState;
-        private Player player1;
-        private Player player2;
-        private Player currentPlayer;
+class Board_3x3 implements Board {
+    private char[][] board;
+    private BoardState boardState;
+    private PlayerState playerState;
 
-        // Constructor
-        Board_3x3(Player player1, Player player2) {
-            this.player1 = player1;
-            this.player2 = player2;
-            this.currentPlayer = player1; // Start with player 1
-
-            board = new char[3][3];
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    board[i][j] = ' '; // Initialize with empty spaces
-                }
+    Board_3x3() {
+        board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' '; //initialize with empty spaces
             }
-            boardState = BoardState.CREATED;
-            playerState = PlayerState.UNDEFINED;
+        }
+        boardState = BoardState.CREATED;
+        playerState = PlayerState.UNDEFINED;
+    }
+
+    @Override
+    public boolean addMove(Player player, int x, int y) {
+        if (x < 0 || x >= 3 || y < 0 || y >= 3 || board[x][y] != ' ') {
+            return false; //invalid move
         }
 
-        private char getCurrentPlayerMark() {
-            return currentPlayer.getMark();
-        }
-
-        private void switchTurn() {
-            currentPlayer = (currentPlayer == player1) ? player2 : player1;
-        }
-
-        @Override
-        public boolean addMove(int x, int y) {
-            char mark = getCurrentPlayerMark();
-
-            if (x >= 0 && x < 3 && y >= 0 && y < 3 && board[x][y] == ' ') {
-                board[x][y] = mark;
-                updateBoardAndPlayerState(x, y, mark);
-                switchTurn(); // Switch the turn after a move
-                return true;
-            }
-            return false;
-        }
-
+        board[x][y] = player.getMark();
+        updateBoardAndPlayerState(x, y, player.getMark());
+        return true;
+    }
 
     @Override
     public char[][] getBoardValues() {
@@ -58,20 +38,16 @@ package game;
     }
 
     @Override
-    public PlayerState getPlayerState() {
+    public PlayerState getPlayerState(Player player) {
         return playerState;
     }
 
-    // Additional private method to update the state of the board and player after a move
     private void updateBoardAndPlayerState(int x, int y, char mark) {
         if (checkWin(x, y, mark)) {
             playerState = PlayerState.WIN;
+            boardState = BoardState.DONE;
         } else if (checkDraw()) {
             playerState = PlayerState.DRAW;
-        }
-
-        // Update the board state based on player state
-        if (playerState == PlayerState.WIN || playerState == PlayerState.DRAW) {
             boardState = BoardState.DONE;
         } else {
             boardState = BoardState.RUNNING;
@@ -79,68 +55,47 @@ package game;
     }
 
     private boolean checkDraw() {
-        // Check if all cells are filled
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                 if (board[i][j] == ' ') {
-                     return false;
-                 }
+                if (board[i][j] == ' ') {
+                    return false;
+                }
             }
         }
-        return true;
+        return true; //no empty spaces = draw
     }
 
-    // Additional private method to check if the game is won
     private boolean checkWin(int x, int y, char mark) {
-        // Check if the row is filled
-        boolean rowFilled = true;
+        //check row and column
+        boolean rowWin = true;
+        boolean colWin = true;
         for (int i = 0; i < 3; i++) {
-            if (board[x][i] != mark) {
-                rowFilled = false;
-                break;
-            }
+            if (board[x][i] != mark) rowWin = false;
+            if (board[i][y] != mark) colWin = false;
         }
-        if (rowFilled) {
-            return true;
-        }
+        if (rowWin || colWin) return true;
 
-        // Check if the column is filled
-        boolean colFilled = true;
+        //check diagonals
+        boolean diagWin = true;
+        boolean antiDiagWin = true;
         for (int i = 0; i < 3; i++) {
-            if (board[i][y] != mark) {
-                colFilled = false;
-                break;
-            }
+            if (board[i][i] != mark) diagWin = false;
+            if (board[i][2 - i] != mark) antiDiagWin = false;
         }
-        if (colFilled) {
-            return true;
-        }
-
-        // Check if the diagonal is filled
-        boolean diagFilled = true;
-        for (int i = 0; i < 3; i++) {
-            if (board[i][i] != mark) {
-                diagFilled = false;
-                break;
-            }
-        }
-        if (diagFilled) {
-            return true;
-        }
-
-        // Check if the anti-diagonal is filled
-        boolean antiDiagFilled = true;
-        for (int i = 0; i < 3; i++) {
-            if (board[i][2 - i] != mark) {
-                antiDiagFilled = false;
-                break;
-            }
-        }
-        if (antiDiagFilled) {
-            return true;
-        }
-
-        return false;
+        return diagWin || antiDiagWin;
     }
 
+    //method to print the board
+    public void printBoard() {
+        System.out.println("Current Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|" + board[i][j]);
+            }
+            System.out.println("|");
+            if (i < 2) {
+                System.out.println("-----------");
+            }
+        }
+    }
 }
--- src/main/java/game/HumanPlayer.java
+++ src/main/java/game/HumanPlayer.java
@@ -3,12 +3,12 @@ package game;
 import common.Result;
 
 class HumanPlayer implements Player {
-    private GameIOImpl io;
+    private GameIO io;
     private String name;
     private char mark;
 
     // Constructor
-    HumanPlayer(GameIOImpl io, String name, char mark) {
+    HumanPlayer(GameIO io, String name, char mark) {
         this.io = io;
         this.name = name;
         this.mark = mark;
@@ -20,34 +20,27 @@ class HumanPlayer implements Player {
     }
 
     @Override
-    public char getMark() {
-        return 0;
-    }
-
-    @Override
     public String getName() {
         return name;
     }
 
     @Override
-    public Result<Boolean, String> makeMove(Board board) {
+    public boolean makeMove(Board board) {
         io.putString("Player " + name + " (" + mark + "), make your move. Enter a number between 1 and 9:");
 
         Result<Integer, String> moveResult = io.getInt();
         if (moveResult.isErr()) {
-            return Result.err(moveResult.getErr());
+            return false;
         }
 
         int move = moveResult.unwrap();
 
-        // Convert the move (1-9) to board coordinates (0-2, 0-2)
-        int x = (move - 1) / 3; // Row
-        int y = (move - 1) % 3; // Column
-
-        if (board.addMove(x, y)) {
-            return Result.ok(true);
-        } else {
-            return Result.err("Invalid move. Please try again.");
+        if (move < 1 || move > 9) {
+            return false;
         }
+        int x = (move - 1) / 3;
+        int y = (move - 1) % 3;
+
+        return board.addMove(this, x, y);
     }
 }
